8d5f987fd11ed1746722e551159e30ac5db6014e
Warhammer99 <82106487+Yasodhyassp30@users.noreply.github.com>
2021-10-16 09:21:37 +0530

    2021-10-16-login-sginup Yaso

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,51 @@
-import React from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUp from "./components/SignUp";
 
 function App() {
+  const [user,setUser]=useState({name:"",email:""});
+  const [error,setError]=useState("")
+  const[logorcreate,setLogorCreate]=useState(false);
+  const Login=(data)=>{
+    setUser({name:data.name,email:data.email});
+  }
+  const Logout=()=>{
+    setUser({name:"",email:""})
+    setError("")
+    setLogorCreate(false)
+  }
+  const Logerror=err=>{
+    setError(err)
+  }
+  const Logorcreate=()=>{
+  if(logorcreate){
+    setLogorCreate(false)
+  }else{
+    setLogorCreate(true)
+  }
+  setError("")
+  }
+
+  
+ 
   return (
-    <div className="App">
-<h1>Hello</h1>
-    </div>
+    <div>
+      {(user.email!=="")?(
+        <div>
+        <h2>Welcome {user.name}</h2>
+        <button onClick={Logout}>Logout</button>
+        </div>
+      ):( (logorcreate==true)?(<div>
+          <SignUp Login={Login} Logerror={Logerror} error={error} check={Logorcreate}/>
+      </div>):(
+        <div>
+           <LoginForm Login={Login} Logerror={Logerror} error={error} check={Logorcreate}/>
+        </div>
+      ))}    
+   </div>  
   );
 }
 
 export default App;
-  
---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+const axios=require('axios').default;
+
+
+function LoginForm({Login,Logerror,error,check}) {
+    const [logindetails,setDetails]=useState({email:"",password:""});
+    async function Auth(event) {
+        event.preventDefault();
+        if(logindetails.email==="" || logindetails.password===""){
+          Logerror("Please Fill all Required Fields")
+        }else{
+        const response = await axios.post("http://localhost:8070/employee/auth/", {
+            email:logindetails.email,
+            password:logindetails.password
+    }).then(function(response){
+      const data=response.data.employee
+      Login(data)
+    }).catch(function(err){
+      Logerror("User Not Exists")
+    });}
+  }
+      
+    return (
+        <form className="col-12 " onSubmit={Auth}>
+        <div className="col-2 offset-5  text-danger">{error}</div>
+        <div className="form-group mt-3 col-2 offset-5">
+        <label for="email">E-mail :</label>
+        <input  className="form-control" onChange={e => setDetails({...logindetails,email:e.target.value})} value={logindetails.email} type="text" placeholder="Email"/>
+        </div>
+        <div className="form-group col-2 offset-5">
+        <label for="password">Password :</label>
+        <input className="form-control" onChange={e => setDetails({...logindetails,password:e.target.value})} value={logindetails.password} type="password" placeholder="Password"/>
+        </div>
+        <br />
+        <input type="submit" className="btn btn-success col-2 offset-5" value="Login" />
+        <input type="button" className="btn btn-success col-2 offset-5 mt-3" value="Create New Account" onClick={check}/>
+      </form>
+    )
+
+}
+
+export default LoginForm
