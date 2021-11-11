892cfa7803ccb949193ff8ab5c784ee96b445c22
Malaka Rodrigo <shehanmalakarodrigo@gmail.com>
2021-11-11 20:56:09 +0530

    redux folder removed.
    Edited imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import{Provider} from 'react-redux';
-import {store} from './redux/store/store';
+
 
 
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
    <div><App /></div> 
-  </Provider>,
+   </React.StrictMode>,
   document.getElementById('root')
 );
