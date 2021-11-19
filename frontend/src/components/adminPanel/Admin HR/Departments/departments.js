504fa21d847194925ca71976cf3b1cd17091d4f5
chanukaabeysinghe1031 <abeysinghechanuka@gmail.com>
2021-11-19 17:10:17 +0530

    Displayed Designations

--- frontend/src/components/adminPanel/Admin HR/Departments/departments.js
+++ frontend/src/components/adminPanel/Admin HR/Departments/departments.js
@@ -13,13 +13,13 @@ class Departments extends Component {
         this.state = {
             departments: [],
             searchTerm: "Department1",
-            department:{
-                department_name:"",
-                department_desc:""
+            department: {
+                department_name: "",
+                department_desc: ""
             },
-            designation:{
-                designation_name:"",
-                designation_desc:"",
+            designation: {
+                designation_name: "",
+                designation_desc: "",
                 department: ""
             }
         }
@@ -38,17 +38,17 @@ class Departments extends Component {
     }
 
     addDepartment = () => {
-        axios.post('http://localhost:8070/departments/addDepartment',this.state.department)
+        axios.post('http://localhost:8070/departments/addDepartment', this.state.department)
             .then((res) => {
                 axios.get('http://localhost:8070/departments/')
                     .then((res) => {
                         this.setState({
                             departments: res.data,
-                            department:{
-                                department_name:"",
-                                department_desc:""
+                            department: {
+                                department_name: "",
+                                department_desc: ""
                             },
-                        }, () => console.log("Departments", this.state.departments,"Department",this.state.department))
+                        }, () => console.log("Departments", this.state.departments, "Department", this.state.department))
                     })
                     .catch(error => {
                         console.log(error)
@@ -60,15 +60,15 @@ class Departments extends Component {
             })
     }
     addDesignation = () => {
-        axios.post('http://localhost:8070/designations/addDesignation',this.state.designation)
+        axios.post('http://localhost:8070/designations/addDesignation', this.state.designation)
             .then((res) => {
                 axios.get('http://localhost:8070/departments/')
                     .then((res) => {
                         this.setState({
                             departments: res.data,
-                            designation:{
-                                designation_name:"",
-                                designation_desc:"",
+                            designation: {
+                                designation_name: "",
+                                designation_desc: "",
                                 department: ""
                             }
                         }, () => console.log("Departments", this.state.departments))
@@ -79,25 +79,25 @@ class Departments extends Component {
                 alert(res.data.message)
             })
             .catch(error => {
-               alert(error.data)
+                alert(error.data)
             })
     }
 
     clickMore = (department) => {
         this.props.history.push({
-            pathname:"/hr/designations",
+            pathname: "/hr/designations",
             search: '?query=abc',
-            state: { detail: department}
+            state: {detail: department}
         })
     }
 
     render() {
         const {departments} = this.state
         return (
-            <div className="designationsMainComponent">
+            <div className="departmentsMainComponent">
                 <HRNavbar/>
                 <Sidebar/>
-                <div className="designationsSubComponent">
+                <div className="departmentsSubComponent">
                     <div className="addDepartmentsComponents">
                         <div className="addDepartmentsSubComponent">
                             <h5 className="hrTitleText">Add Departments</h5>
@@ -107,21 +107,27 @@ class Departments extends Component {
                                         Department Name
                                         <input className="hrTextInput" type="text" name="dName"
                                                value={this.state.department.department_name}
-                                               onChange={e=>this.setState({
-                                                   department:{...this.state.department,department_name:e.target.value}
+                                               onChange={e => this.setState({
+                                                   department: {
+                                                       ...this.state.department,
+                                                       department_name: e.target.value
+                                                   }
                                                })}/>
                                     </label>
                                     <label className="hrLabel">
                                         Department Description
                                         <input className="hrTextInput" type="text" name="dDesc"
                                                value={this.state.department.department_desc}
-                                               onChange={e=>this.setState({
-                                                   department:{...this.state.department,department_desc:e.target.value}
+                                               onChange={e => this.setState({
+                                                   department: {
+                                                       ...this.state.department,
+                                                       department_desc: e.target.value
+                                                   }
                                                })}/>
                                     </label>
                                 </div>
-                                <div className="designations_addButtonContainer" onClick={this.addDepartment}>
-                                    <h7 className="designations_addButton">Add Department</h7>
+                                <div className="departments_addButtonContainer" onClick={this.addDepartment}>
+                                    <h7 className="departments_addButton">Add Department</h7>
                                 </div>
                             </form>
                         </div>
@@ -134,92 +140,100 @@ class Departments extends Component {
                                         Designation Name
                                         <input className="hrTextInput" type="text" name="name"
                                                value={this.state.designation.designation_name}
-                                               onChange={e=>this.setState({
+                                               onChange={e => this.setState({
 
-                                                   designation:{designation_name:e.target.value}})}/>
+                                                   designation: {designation_name: e.target.value}
+                                               })}/>
                                     </label>
                                     <label className="hrLabel">
                                         Designation Description
                                         <input className="hrTextInput" type="text" name="name"
-                                               onChange={e=>this.setState({
-                                                   designation:{...this.state.designation,designation_desc:e.target.value}
+                                               onChange={e => this.setState({
+                                                   designation: {
+                                                       ...this.state.designation,
+                                                       designation_desc: e.target.value
+                                                   }
                                                })}/>
                                     </label>
 
                                     <label className="hrLabel">
                                         Department
-                                        <select className="form-select form-select-sm dropdownbg " defaultValue={""}
-                                                onChange={e=>this.setState({
-                                                    designation:{...this.state.designation,department:e.target.value}
+                                        <select className="form-select form-select-sm departments_select "
+                                                defaultValue={""}
+                                                onChange={e => this.setState({
+                                                    designation: {...this.state.designation, department: e.target.value}
                                                 })}
                                         >
-                                            <option disabled  value={""} > -- Select a Department -- </option>
+                                            <option disabled value={""}> -- Select a Department --</option>
                                             {this.state.departments.map(item => {
-                                                return (<option  key={item.Department._id} value={item.Department._id}>{item.Department.department_name}</option>);
+                                                return (<option key={item.Department._id}
+                                                                value={item.Department._id}>{item.Department.department_name}</option>);
                                             })}
                                         </select>
                                     </label>
                                 </div>
-                                <div className="designations_addButtonContainer">
-                                    <h7 className="designations_addButton" onClick={this.addDesignation}>Add Designation</h7>
+                                <div className="departments_addButtonContainer">
+                                    <h7 className="departments_addButton" onClick={this.addDesignation}>Add
+                                        Designation
+                                    </h7>
                                 </div>
                             </form>
                         </div>
                     </div>
 
                     <div className="departments_table_view">
-                        <table className="designationsTable">
-                            <tr className="designations_table_head">
-                                <th className="designations_table_header_column">Department</th>
-                                <th className="designations_table_header_column">Description</th>
-                                <th className="designations_table_header_column">More</th>
+                        <table className="departmentsTable">
+                            <tr className="departments_table_head">
+                                <th className="departments_table_header_column">Department</th>
+                                <th className="departments_table_header_column">Description</th>
+                                <th className="departments_table_header_column">More</th>
                             </tr>
 
                             {(departments.length > 0) ? departments.map((department, index) => {
-                                if (index % 2 == 0) {
-                                    return (
-                                        <tr className="designations_table_data_odd" key={index}>
-                                            <td className="designations_table_data_column">{department.Department.department_name}</td>
-                                            <td className="designations_table_data_column">{department.Department.department_desc}</td>
-                                            <td className="designations_table_data_column_more">
-                                                <div
-                                                    className="moreButton"
-                                                    onClick={() => this.clickMore(department)}
-                                                >
-                                                    More
-                                                </div>
-                                            </td>
-                                        </tr>
+                                    if (index % 2 == 0) {
+                                        return (
+                                            <tr className="departments_table_data_odd" key={index}>
+                                                <td className="departments_table_data_column">{department.Department.department_name}</td>
+                                                <td className="departments_table_data_column">{department.Department.department_desc}</td>
+                                                <td className="departments_table_data_column_more">
+                                                    <div
+                                                        className="moreButton"
+                                                        onClick={() => this.clickMore(department)}
+                                                    >
+                                                        More
+                                                    </div>
+                                                </td>
+                                            </tr>
 
-                                    )
-                                } else {
-                                    return (
-                                        <tr className="designations_table_data_even" key={index}>
-                                            <td className="designations_table_data_column">{department.Department.department_name}</td>
-                                            <td className="designations_table_data_column">{department.Department.department_desc}</td>
-                                            <td className="designations_table_data_column_more">
-                                                <div
-                                                    className="moreButton"
-                                                    onClick={() => this.clickMore(department)}
-                                                >
-                                                    More
-                                                </div>
-                                            </td>
-                                        </tr>
+                                        )
+                                    } else {
+                                        return (
+                                            <tr className="departments_table_data_even" key={index}>
+                                                <td className="departments_table_data_column">{department.Department.department_name}</td>
+                                                <td className="departments_table_data_column">{department.Department.department_desc}</td>
+                                                <td className="departments_table_data_column_more">
+                                                    <div
+                                                        className="moreButton"
+                                                        onClick={() => this.clickMore(department)}
+                                                    >
+                                                        More
+                                                    </div>
+                                                </td>
+                                            </tr>
 
-                                    )
-                                }
-                            }) :
+                                        )
+                                    }
+                                }) :
                                 <tr>
                                     <td colSpan="5">Loading...</td>
                                 </tr>
                             }
-                                </table>
-                                </div>
-                                </div>
-                                </div>
-                                );
-                            }
-                            }
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-                            export default withRouter(Departments)
+export default withRouter(Departments)
--- frontend/src/components/adminPanel/Admin HR/Designations/designations.js
+++ frontend/src/components/adminPanel/Admin HR/Designations/designations.js
@@ -5,23 +5,78 @@ import Sidebar from "./sidebar";
 import "./designations_style.css"
 import HRNavbar from "../HRNavBar/hr_navbar";
 
-export  default  class Designations extends Component {
+class Designations extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            department : this.props.location.state.detail
+        }
     }
 
     componentDidMount() {
+        console.log(this.state.department)
     }
 
     render(){
+        const {department} = this.state
         return(
             <div className="designationsMainComponent">
                 <HRNavbar/>
                 <Sidebar/>
                 <div className="designationsSubComponent">
-                    <h5 className="hrText">This is the Designations Component</h5>
+                    <h5 className="hrText">{department.Department.department_name}</h5>
+                    <div className="departments_table_view">
+                        <table className="designationsTable">
+                            <tr className="designations_table_head">
+                                <th className="designations_table_header_column">Designation</th>
+                                <th className="designations_table_header_column">Description</th>
+                                <th className="designations_table_header_column">Delete</th>
+                            </tr>
+
+                            {(department.Designations.length > 0) ? department.Designations.map((designation, index) => {
+                                    if (index % 2 == 0) {
+                                        return (
+                                            <tr className="designations_table_data_odd" key={index}>
+                                                <td className="designations_table_data_column">{designation.designation_name}</td>
+                                                <td className="designations_table_data_column">{designation.designation_desc}</td>
+                                                <td className="designations_table_data_column_more">
+                                                    <div
+                                                        className="moreButton"
+                                                        onClick={() => this.delete(designation)}
+                                                    >
+                                                        Delete
+                                                    </div>
+                                                </td>
+                                            </tr>
+
+                                        )
+                                    } else {
+                                        return (
+                                            <tr className="designations_table_data_even" key={index}>
+                                                <td className="designations_table_data_column">{designation.designation_name}</td>
+                                                <td className="designations_table_data_column">{designation.designation_desc}</td>
+                                                <td className="designations_table_data_column_more">
+                                                    <div
+                                                        className="moreButton"
+                                                        onClick={() => this.delete(designation)}
+                                                    >
+                                                        Delete
+                                                    </div>
+                                                </td>
+                                            </tr>
+
+                                        )
+                                    }
+                                }) :
+                                <tr>
+                                    <td colSpan="5">Loading...</td>
+                                </tr>
+                            }
+                        </table>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+export default withRouter(Designations);
